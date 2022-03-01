04bc1db2cc1b2b96b44a6bbc9ce53ab5cf519c07
Vaibhav Singh <74946719+VAIBHAV0526@users.noreply.github.com>
2022-03-01 09:18:37 +0530

    Add files via upload

--- ser.cpp
+++ ser.cpp
@@ -1,23 +1,41 @@
 #include "bits/stdc++.h"
 using namespace std;
 typedef unsigned long long ull;
-void local(){
-	 #ifndef ONLINE_JUDGE
- 
-    // For getting input from input.txt file
-    freopen("input.txt", "r", stdin);
- 
-    // Printing the Output to output.txt file
-    freopen("/Users/vaibhavsingh/Desktop/code/output.txt", "w", stdout);
- 
-#endif
-}
-int main(){
-    ull x=4,y=4;
-    //cin>>x>>y;
-    if(!(x&1)){
-        cout<<x*x+y+1;
+#define lli long long int
+#define li long int
+#define ld long double
+#define MOD 1000000007
+using namespace std;
+
+int fact(int n){
+    if(n==0 || n==1){
+        return 1;
     }
-    else{
-    cout<<y*y-x+1;
-}}
+    return n*fact(n-1);
+}
+
+signed main(){
+
+string s;
+cin>>s;
+// we will next_permutation
+sort(s.begin(), s.end());
+vector<string> res;
+do{
+    res.push_back(s);
+}
+while(next_permutation(s.begin(), s.end()));
+cout<<res.size()<<endl;
+for(auto i: res){
+    cout<<i<<endl;
+}
+
+
+
+
+
+
+
+
+ return 0;
+}
